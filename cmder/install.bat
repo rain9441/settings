844b853991f8d91902719662850a74e7967d235a
rain9441 <rain9441@gmail.com>
2024-04-08 12:23:46 -0500

    2024 update - cmder settings

--- cmder/install.bat
+++ cmder/install.bat
@@ -7,10 +7,7 @@ RETURN 0"
 
 :exists
 
-CP -r "%~dp0\settings" "C:/Program Files/Cmder/config/"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-CP -r "%~dp0\user-ConEmu.xml" "C:/Program Files/Cmder/config/"
+CP -r "%~dp0\ConEmu.xml" "C:/Program Files/Cmder/vendor/conemu-maximus5/"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo cmder settings installed successfully
